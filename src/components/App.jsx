a4dfe6cc1daead4cbb7ef95513635b9ba76a2acf
prashanthsasidharan <prashanthsasidharan123@gmail.com>
2023-02-22 20:45:21 +0530

    added blogs section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,16 +5,26 @@ import Projects from './Projects/Projects';
 import Skills from './Skills/Skills';
 import Contact from './Contact/Contact';
 import Footer from './Footer/Footer';
+import Blogs from './Blogs/Blogs';
 
 import { PortfolioProvider } from '../context/context';
 
-import { heroData, aboutData, projectsData, skillsData, contactData, footerData } from '../mock/data';
+import {
+  heroData,
+  aboutData,
+  projectsData,
+  skillsData,
+  contactData,
+  footerData,
+  blogsData,
+} from '../mock/data';
 
 function App() {
   const [hero, setHero] = useState({});
   const [about, setAbout] = useState({});
   const [projects, setProjects] = useState([]);
   const [skills, setSkills] = useState([]);
+  const [blogs, setBlogs] = useState([]);
   const [contact, setContact] = useState({});
   const [footer, setFooter] = useState({});
 
@@ -23,17 +33,18 @@ function App() {
     setAbout({ ...aboutData });
     setProjects([...projectsData]);
     setSkills([...skillsData]);
+    setBlogs([...blogsData]);
     setContact({ ...contactData });
     setFooter({ ...footerData });
   }, []);
 
   return (
-    <PortfolioProvider value={{ hero, about, projects, skills, contact, footer }}>
+    <PortfolioProvider value={{ hero, about, projects, skills, blogs, contact, footer }}>
       <Hero />
       <About />
       <Projects />
       <Skills />
-      <Projects />
+      <Blogs />
       <Contact />
       <Footer />
     </PortfolioProvider>
--- src/mock/data.js
+++ src/mock/data.js
@@ -125,6 +125,101 @@ export const skillsData = [
   },
 ];
 
+// BLOGS DATA
+
+export const blogsData = [
+  {
+    id: nanoid(),
+    img: 'blogs/execution-context.jpg',
+    title: 'How Does JS code run - Execution context and Call stack',
+    info: 'Learn how execution context works and how the order of execution context is maintained by call stack.',
+    pills: ['Call stack', 'Execution Context', 'Browser'],
+    url: 'https://dev.to/prashan81992916/how-does-js-code-run-execution-context-and-call-stack-3a7',
+    date: '11 May 2021',
+  },
+  {
+    id: nanoid(),
+    img: 'blogs/js-logo.jpg',
+    title: 'Know about let, var and const in JS',
+    info: 'Sometimes understanding let, var, const and their scopes can be confusing. Know how they exactly work by clicking the above link',
+    pills: ['Snippet', 'Scope'],
+    url: 'https://www.linkedin.com/feed/update/urn:li:activity:6902831857168203776/',
+    date: '2 Feb 2022',
+  },
+  {
+    id: nanoid(),
+    img: 'blogs/computed-prop.jpg',
+    title: 'Computed property names',
+    info: 'Computed property names was introduced in ES5, which allows to provide expressions inside the brackets. So throught this we can dynamically set or get a prop after computing expression inside the bracket',
+    pills: ['Snippet', 'Computed Props'],
+    url: 'https://www.linkedin.com/feed/update/urn:li:activity:6902831857168203776/',
+    date: '2 Feb 2022',
+  },
+  {
+    id: nanoid(),
+    img: 'blogs/mixins.jpg',
+    title: 'Mixins Pattern',
+    info: 'Mixin is an object which can used to add additional functionality to an object or class',
+    pills: ['Mixins', 'Javascript', 'Design Patterns'],
+    url: 'https://www.linkedin.com/posts/prashanth-sasidharan-7a32301a8_mixin-js-javascript-activity-6905861264082386944-6ZQh/?utm_source=share&utm_medium=member_desktop',
+    date: '24 Mar 2022',
+  },
+  {
+    id: nanoid(),
+    img: 'blogs/proxy.jpg',
+    title: 'Proxy pattern',
+    info: 'Proxy intercepts and controls interactions to target objects.',
+    pills: ['Proxy', 'Javascript', 'Design Patterns'],
+    url: 'https://www.linkedin.com/posts/prashanth-sasidharan-7a32301a8_proxy-js-javascript-activity-6902270038393409536-foXy/?utm_source=share&utm_medium=member_desktop',
+    date: '29 Mar 2022',
+  },
+  {
+    id: nanoid(),
+    img: 'blogs/modules.jpg',
+    title: 'Modules in Js',
+    info: 'Learn about code splitting, dynamic imports how modules patterns works in JS',
+    pills: ['Dynamic Imports', 'Javascript', 'Design Patterns'],
+    url: 'https://www.linkedin.com/posts/prashanth-sasidharan-7a32301a8_modules-js-javascript-activity-6904408738531295232-cxmR/?utm_source=share&utm_medium=member_desktop',
+    date: '15 Apr 2022',
+  },
+  {
+    id: nanoid(),
+    img: 'blogs/singleton.jpg',
+    title: 'Singelton Pattern',
+    info: 'Class that is instantiated only once during the runtime of the application in the global scope',
+    pills: ['Singelton', 'Javascript', 'Design Patterns'],
+    url: 'https://www.linkedin.com/posts/prashanth-sasidharan-7a32301a8_singleton-js-javascript-activity-6901963539494371328-ZfcT/?utm_source=share&utm_medium=member_desktop',
+    date: '23 Apr 2022',
+  },
+  {
+    id: nanoid(),
+    img: 'blogs/memoize.jpg',
+    title: 'Memoization method',
+    info: 'This is an experimentation post on memoizing function and invokes the function only if anyone of the dependency property values changes',
+    pills: ['Memo', 'Javascript', 'Design Patterns'],
+    url: 'https://www.linkedin.com/feed/update/urn:li:activity:6896812653507952640/?utm_source=share&utm_medium=member_desktop',
+    date: '30 Apr 2022',
+  },
+  {
+    id: nanoid(),
+    img: 'blogs/drum.jpg',
+    title: 'Virtual Drum Kit using Javascript',
+    info: 'Learn to create simple virtual drum using vanilla js and learn about transitions and data attributes',
+    pills: ['Fun Project', 'Web Apis'],
+    url: 'https://dev.to/prashan81992916/build-a-virtual-drum-kit-using-javascript-203k',
+    date: '18 Jul 2021',
+  },
+  {
+    id: nanoid(),
+    img: 'blogs/default-prop.jpg',
+    title: 'Dont assign props as the default value for useState',
+    info: 'Know how to correctly assign props from the parent component as a default value in the childs useState hook',
+    pills: ['React', 'bug'],
+    url: 'https://www.linkedin.com/feed/update/urn:li:activity:7026952728945848320/',
+    date: '30 Apr 2022',
+  },
+];
+
 // CONTACT DATA
 export const contactData = {
   cta: '',
@@ -160,5 +255,5 @@ export const footerData = {
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false,
 };
